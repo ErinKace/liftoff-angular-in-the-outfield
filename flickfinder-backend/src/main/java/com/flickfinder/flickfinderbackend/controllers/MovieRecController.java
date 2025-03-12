77c41e58fda18cff291a05ff4f8f99647b0ffdfc
ErinKace <erinkacerovskis@gmail.com>
2025-03-11 21:42:42 -0500

    added movie rec controller

---
+++ flickfinder-backend/src/main/java/com/flickfinder/flickfinderbackend/controllers/MovieRecController.java
@@ -0,0 +1,24 @@
+package com.flickfinder.flickfinderbackend.controllers;
+
+import com.flickfinder.flickfinderbackend.models.Movie;
+import com.flickfinder.flickfinderbackend.services.MovieService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/rec")
+public class MovieRecController {
+
+    private MovieService movieService;
+    public MovieRecController(MovieService movieService) {
+        this.movieService = movieService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Flux<Movie>> getMovieRec() {
+
+    }
+}
